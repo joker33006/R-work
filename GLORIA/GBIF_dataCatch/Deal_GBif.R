f99ef0fb95469016437b7e143810d6bb6000555c
joker33006 <joker33006@gmail.com>
2020-05-19 15:47:54 +0800

    change to paral model

--- GLORIA/GBIF_dataCatch/Deal_GBif.R
+++ GLORIA/GBIF_dataCatch/Deal_GBif.R
@@ -1,12 +1,23 @@
 library(data.table)
 library(rgbif)
+library(ggplot2)
 setwd("E:/忍者/R-work/Git/R-work/GLORIA/GBIF_dataCatch/prim_result")
 sp_p <- list.files("E:/忍者/R-work/Git/R-work/GLORIA/GBIF_dataCatch/prim_result", pattern='.csv')
 my_data <- lapply(sp_p,fread)
 loss <- sub('.csv',"",sp_p[lapply(my_data,nrow)<30])
-my_data[lapply(my_data,nrow)<30]
 write.csv(loss,"E:/忍者/R-work/Git/R-work/GLORIA/GBIF_dataCatch/loss_name.csv")
 ################ checking the loss name and completing the data
-
-
-
+sp_n <- sub('.csv',"",sp_p)
+for (i in 1:72){
+  my_data[[i]][,name:=sp_n[i]][,code:=as.numeric(i)]
+}
+bio <- NULL
+bio_tol <- NULL
+for (i in 1:72){
+bio <- my_data[[i]]
+bio_tol <- rbind(bio_tol,bio)
+}
+bio_tol <- bio_tol[bio_1!="NA"]
+#############################plot
+ggplot(bio_tol,aes(factor(code),bio_12))+geom_boxplot()
+class(bio_tol[,code])
--- GLORIA/GBIF_dataCatch/GetGBIF.R
+++ GLORIA/GBIF_dataCatch/GetGBIF.R
@@ -1,30 +1,40 @@
 library(rgbif)
 library(data.table)
 library(raster)
+library(tictoc) # running time
+library(parallel) #平行運算
 setwd("E:/忍者/R-work/Git/R-work/GLORIA/GBIF_dataCatch")
 svpath <- "E:/忍者/R-work/Git/R-work/GLORIA/GBIF_dataCatch"
 path <- 'E:/Climdata/Chelsa_current_2019'
 clim_d_n <- list.files(path)
-nlist <-read.csv("loss_list.txt")
+nlist <-read.csv("name_list.txt")
+cl <- makeCluster(6)
 for (j in 1:length(nlist[,1])){
-  name <- as.character(nlist[j,1])
+  tic("total")
+    name <- as.character(nlist[j,1])
   ##### start extract the climate data
   result <- NULL
 
   for (i in 1:length(clim_d_n)){
+    tic("climate")
     rast <- raster(paste0(path,'/',clim_d_n[i]))
     data <- as.data.table(occ_search(scientificName = name)$data)
     if (nrow(data)==0){name <- paste0('NULL_',name)
     break
     }else{
-      spot <- data[decimalLatitude!="NA",.(decimalLongitude,decimalLatitude)]
-      x <- extract(rast,spot)
+      spot <- data[decimalLatitude!="NA",.(decimalLongitude,decimalLatitude,key)]
+      dot <- spot[,.(decimalLongitude,decimalLatitude)]
+      x <- extract(rast,dot)
       result <-cbind(result,x) 
      colnames(result)[i] <- paste0("bio_",i)
-     }
+     toc()
+    }
   }
+  result <- cbind(spot[,.(decimalLongitude,decimalLatitude,key)],result)
 write.csv(result,paste0(svpath,"/prim_result/",name,".csv"))
 cat(j)
+toc()
+
 }
 #deal with the primary data
 
